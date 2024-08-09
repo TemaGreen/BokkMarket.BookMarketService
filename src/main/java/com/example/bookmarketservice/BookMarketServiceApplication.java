af46592649d18729c3a0838cd7cbfeafc02e8bfa
TemaGreen1997 <arhamidullin@gmail.com>
2024-08-10 03:55:27 +0400

    init

---
+++ src/main/java/com/example/bookmarketservice/BookMarketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmarketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookmarketservice/BookMarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmarketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
